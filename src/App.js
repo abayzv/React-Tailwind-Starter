028f0df4ae53719eb75b8615bf5e53d93b7e1f2c
unknown <ajipunk008@gmail.com>
2022-07-27 15:35:17 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+          class="p-4 mb-4 text-sm text-blue-700 bg-blue-100 rounded-lg dark:bg-blue-200 dark:text-blue-800"
+          role="alert"
         >
-          Learn React
-        </a>
+          <span class="font-medium">Info alert!</span> Change a few things up
+          and try submitting again.
+        </div>
       </header>
     </div>
   );
